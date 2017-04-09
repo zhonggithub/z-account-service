1478942e05a3b5e95ae8353105d0933dd0f37245
zhonggithub <1006817093@qq.com>
2017-04-09 21:51:14 +0800

    修改代码

--- src/initDataBase.js
+++ src/initDataBase.js
@@ -1,13 +1,14 @@
 /*
  * @Author: Zz
  * @Date: 2017-04-09 21:43:06
- * @Last Modified by:   Zz
- * @Last Modified time: 2017-04-09 21:43:06
+ * @Last Modified by: Zz
+ * @Last Modified time: 2017-04-09 21:50:44
  */
 import initDB from './initDB';
 
 initDB((error) => {
   if (error) {
+    console.log(error);
     throw error;
   }
   console.log('init db success!');
